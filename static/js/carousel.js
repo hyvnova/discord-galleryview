dabccb006d0dbc379a94afa21bc2046860b210e7
EZSNoVa <thenovaggamer@gmail.com>
2023-08-03 13:50:37 -0400

    Finishing carousel

--- static/js/carousel.js
+++ static/js/carousel.js
@@ -1,20 +1,37 @@
 
-function make_carousel(container_id = "carousel_preview", height = null) {
+
+function make_carousel(container_id = "carousel_preview") {
     let container = document.getElementById(container_id);
-    let image_height = height ? height : document.getElementById("carousel_image_height").value + "vh";
 
     // create and insert buttons (prev, next)
     container.innerHTML += `
-        <div id="carousel_image_container" class="w-auto h-full overflow-hidden"></div>
+<div class="relative container mt-2 w-full flex flex-col justify-center items-center max-w-screen-md mx-auto">
+    <!--Carousel items-->
+    <div id="carousel_item" class="container max-w-screen-md mx-auto px-4 overflow-hidden relative w-auto after:clear-both after:block after:content-['']">
+    </div>
 
-        <button id="prev" class="w-12 h-12 rounded-full bg-gray-200 hover:bg-gray-300 transition-all duration-300 ease-in-out flex justify-center items-center">
-        <i class="fas fa-chevron-left text-gray-700"></i>
+    <!--Carousel controls - prev item-->
+    <button
+        id="carousel_prev"
+        class="absolute bottom-0 left-0 top-0 z-[0] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
+        type="button">
+        <span class="inline-block h-8 w-8">
+            <i class="fas fa-chevron-left"></i>
+        </span>
+        <span
+            class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">Previous</span>
     </button>
-
-    <button id="next" class="w-12 h-12 rounded-full bg-gray-200 hover:bg-gray-300 transition-all duration-300 ease-in-out flex justify-center items-center">
-        <i class="fas fa-chevron-right text-gray-700"></i>
+    <!--Carousel controls - next item-->
+    <button
+        id="carousel_next" class="absolute bottom-0 right-0 top-0 z-[0] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
+        type="button">
+        <span class="inline-block h-8 w-8">
+            <i class="fas fa-chevron-right"></i>
+        </span>
+        <span
+            class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">Next</span>
     </button>
-
+</div>
     `;
 
 
@@ -28,54 +45,48 @@ function make_carousel(container_id = "carousel_preview", height = null) {
         img.src = image[0];
         img.alt = image[1];
         img.name = image[1];
-        img.classList.add("w-full", "rounded-lg", "object-cover", "object-center");
-        img.style.height = image_height;
+        img.classList.add("relative", "float-left", "-mr-[100%]", "max-w-1/2", "h-auto");
 
         // add to array
         carousel_images.push(img);
     });
 
+    let carousel_item = container.querySelector("#carousel_item");
+
     // insert first image
-    container.querySelector("#carousel_image_container").appendChild(carousel_images[current_image_index]);
+    carousel_item.appendChild(carousel_images[current_image_index]);
 
-    // add event listeners to buttons
-    container.querySelector("#prev").addEventListener("click", () => {
+    // prepare buttons
+    let prev_button = container.querySelector("#carousel_prev");
+    let next_button = container.querySelector("#carousel_next");
+
+
+    // add event listeners
+    prev_button.addEventListener("click", () => {
         // remove current image
-        container.querySelector("#carousel_image_container").innerHTML = "";
+        carousel_item.removeChild(carousel_images[current_image_index]);
 
-        // get previous image
+        // add previous image
         current_image_index = current_image_index - 1 < 0 ? carousel_images.length - 1 : current_image_index - 1;
-
-        // insert previous image
-        container.querySelector("#carousel_image_container").appendChild(carousel_images[current_image_index]);
-
+        carousel_item.appendChild(carousel_images[current_image_index]);
     });
 
-    container.querySelector("#next").addEventListener("click", () => {
+    next_button.addEventListener("click", () => {
         // remove current image
-        container.querySelector("#carousel_image_container").innerHTML = "";
+        carousel_item.removeChild(carousel_images[current_image_index]);
 
-        // get next image
+        // add next image
         current_image_index = current_image_index + 1 >= carousel_images.length ? 0 : current_image_index + 1;
-
-        // insert next image
-        container.querySelector("#carousel_image_container").appendChild(carousel_images[current_image_index]);
-
+        carousel_item.appendChild(carousel_images[current_image_index]);
     });
 
-
-    // save height 
-    localStorage.setItem("carousel_image_height", height);
 }
 
 
 function use_carousel() {
     //get height
-    let height = localStorage.getItem("carousel_image_height") ? localStorage.getItem("carousel_image_height") : `${document.getElementById("carousel_image_height").value}%`;
+    gallery.innerHTML = `<div id="gallery_carousel" class="container max-w-screen-md mx-auto overflow-hidden flex items-center justify-center"></div>
+    `;
 
-    gallery.innerHTML = `<div id="gallery_carousel" class="container mt-2 w-10/12  flex justify-center items-center max-h-full">
-    </div>
-    `
-
-    make_list("gallery_carousel", height);
+    make_carousel("gallery_carousel");
 }
